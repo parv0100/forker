f5ad08c73296c8def1081cd9460cb54f64106190
parvindersingh010 <laviesingh45@gmail.com>
2022-04-23 00:08:57 +0530

    key change

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL = 'https://forkify-api.hderokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '1d78e791-c822-4d4d-800d-ffb4b7290162';
+export const KEY = '1d78e791-c82-4d4d-800d-ffb4b7290162';
